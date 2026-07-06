e65858ad3709155d16d358caae0d7a9c5f89970a
devYuno <annykessy20@gmail.com>
2026-07-06 09:45:54 -0300

    FEAT: add bosch fonts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Login } from './Pages/Login'
 import { Discipline } from './Pages/Discipline'
 import { Class } from './Pages/Class'
+import { Content } from './Pages/Content'
 
 function App() {
 
@@ -12,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login/>}></Route>
           <Route path="/Disciplines" element={<Discipline/>}></Route>
-          {/* <Route path="/Content" element={<Content/>}></Route> */}
+          <Route path="/Content" element={<Content/>}></Route>
           <Route path="/Class/{id}" element={<Class/>}></Route>
         </Routes>
       </BrowserRouter>
--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -4,7 +4,13 @@ export function Header() {
     return (
         <>
             <div className="header">
-                <img src="../../public/supergraphic.svg" alt="" />
+                <div className="supergraphic">
+                    <img src="../../public/supergraphic.svg" alt="supergraphic bosch" />
+                </div>
+                <div className="bar">
+                    <img src="../../public/BoschLogo.png" alt="bosch logo"></img>
+                    <i></i>
+                </div>
             </div>
         </>
     )
