5e918c13fb84d09d3f3a1f60c07f556543ef3c59
thaiiss7 <thaismichel7@gmail.com>
2026-07-06 09:16:15 -0300

    feat: add get, delete and put services to student and instructor

--- backend/src/application/services/UserService.ts
+++ backend/src/application/services/UserService.ts
@@ -1,5 +1,5 @@
 import { prisma } from "../../infrastructure/lib/prisma.js";
-import { registerInstrutorDTO, registerStudentDTO, showInstrutorDTO, showStudentDTO } from "../dtos/userDTO.js";
+import { registerInstructorDTO, registerStudentDTO, showInstructorDTO, showStudentDTO, updateInstructorDTO, updateStudentDTO } from "../dtos/userDTO.js";
 
 export const registerStudent = async (data: registerStudentDTO) => {
     const { name, username, password, userType, course } = data
@@ -10,7 +10,7 @@ export const registerStudent = async (data: registerStudentDTO) => {
     })
 }
 
-export const registerInstrutor = async (data: registerInstrutorDTO) => {
+export const registerInstructor = async (data: registerInstructorDTO) => {
     const { name, username, password, userType, specialty, active } = data
     const createdAt = Date.now()
 
@@ -43,7 +43,7 @@ export const showStudent = async (id: number, data: showStudentDTO) => {
     })
 }
 
-export const showInstructor = async (id: number, data: showInstrutorDTO) => {
+export const showInstructor = async (id: number, data: showInstructorDTO) => {
     const { name, username, userType, specialty, active } = data
 
     return await prisma.Instrutor.find({
@@ -57,5 +57,54 @@ export const showInstructor = async (id: number, data: showInstrutorDTO) => {
             specialty: specialty,
             active: active
         }  
+    })
+}
+
+export const deleteStudent = async(id: number) => {
+    return await prisma.Student.delete({
+        where: {
+            id: id
+        }
+    })
+}
+
+export const deleteInstructor = async(id: number) => {
+    return await prisma.Instructor.delete({
+        where: {
+            id: id
+        }
+    })
+}
+
+export const updateStudent = async(id: number, data: updateStudentDTO) => {
+    const { name, username, password, course } = data
+
+    return await prisma.Student.update({
+        where: {
+            id: id
+        },
+        data: {
+            name: name,
+            username: username,
+            password: password,
+            course: course
+        }
+    })
+}
+
+export const updateInstructor = async(id: number, data: updateInstructorDTO) => {
+    const { name, username, password, specialty, active } = data
+
+    return await prisma.Student.update({
+        where: {
+            id: id
+        },
+        data: {
+            name: name,
+            username: username,
+            password: password,
+            specialty: specialty,
+            active: active
+        }
     })
 }
