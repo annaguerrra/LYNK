8c95852c47e3d6073fc0bb11651ccbf53ee20000
annaguerrra <annaguerracastro@gmail.com>
2026-07-17 09:07:12 -0300

    feat: create, assigncompetence services - discipline

--- backend/src/application/dtos/disciplineDTO.ts
+++ backend/src/application/dtos/disciplineDTO.ts
@@ -1,3 +1,52 @@
 export interface DisciplineDTO{
-    
+    name: string
+    workload: number
+    areaID: number
+    userID: number
+}
+
+export interface assignCompetencyDTO{
+    disciplineID: number
+    competencyID: number
+}
+
+export interface findAllDTO{
+    name: string
+    area:{
+        areaID: number;
+        name: number;
+    }
+    competences:{
+        competencyID: number;
+        name: string;
+    }[];
+    classes:{
+        classID: number;
+        name: string;
+    }[];
+    materials:{
+        materialID: number;
+        name: string;
+    }[];
+    exams:{
+        examID: number;
+        name: string
+    }
+}
+
+export interface viewMaterialsDTO{
+    materialID: number
+}
+
+export interface viewCompetencesDTO{
+    competences:{
+        name: string
+        numOfClasses: number
+    }[];
+}
+
+export interface editDisciplineDTO{
+    name: string
+    numOfClasses: string
+    lastUpdate?: Date
 }
