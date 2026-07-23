a1095bf2f54c08a06e7d5e30e7d3c066f35170fa
annaguerrra <annaguerracastro@gmail.com>
2026-07-23 09:43:24 -0300

    doc: commented on exam, log, material and user

--- backend/src/api/routes/examRoutes.ts
+++ backend/src/api/routes/examRoutes.ts
@@ -10,12 +10,19 @@ const router: Router = express.Router()
 const examController = new ExamController()
 
 router
+    // creates an exam
     .post('/exam/create', validateRegister, authorize(UserType.ADMIN, UserType.INSTRUCTOR), examController.register.bind(examController))
+    // allows downloading an exam
     .get('/exams/download/:id/:examAttachmentId', examController.download.bind(examController))
+    // returns all exams without any filter
     .get('/exams', authorize(UserType.ADMIN, UserType.INSTRUCTOR), examController.showExams.bind(examController))
+    // returns a specific exam by id
     .get('/exam/:id', authorize(UserType.ADMIN, UserType.INSTRUCTOR), examController.getExam.bind(examController))
+    // to update an exam's data
     .put('/exam/edit/:id', validateUpdate, authorize(UserType.ADMIN, UserType.INSTRUCTOR), examController.update.bind(examController))
+    // to attach an exam file
     .put('/exam/attachfile', validateAttach, authorize(UserType.ADMIN, UserType.INSTRUCTOR), examController.attachFile.bind(examController))
+    // to delete an specific exam by id
     .delete('/exam/delete/:id', authorize(UserType.ADMIN, UserType.INSTRUCTOR), examController.delete.bind(examController))
 
 export default router
--- backend/src/api/routes/materialRoutes.ts
+++ backend/src/api/routes/materialRoutes.ts
@@ -10,10 +10,15 @@ const router: Router = express.Router()
 const materialController = new MaterialController()
 
 router
+    // creates a material
     .post('/material/create', validateRegister, authorize(UserType.ADMIN, UserType.INSTRUCTOR), materialController.register.bind(materialController))
+    // returns a specific material by id
     .get('/material/:id', materialController.getMaterial.bind(materialController))
+    // allows update information of a material, by id
     .put('/material/edit/:id', validateUpdate, authorize(UserType.ADMIN, UserType.INSTRUCTOR), materialController.update.bind(materialController))
+    // upload a material
     .put('/material/attach', validateAttach, authorize(UserType.ADMIN, UserType.INSTRUCTOR), materialController.attachFile.bind(materialController))
+    // delete a material
     .delete('/material/delete/:id', authorize(UserType.ADMIN, UserType.INSTRUCTOR), materialController.delete.bind(materialController))
 
 export default router
