2b78fcf8f5b3fe1c73d094ef0ab292c049477680
annaguerrra <annaguerracastro@gmail.com>
2026-07-24 15:36:57 -0300

    perf: jwt status response and validation

--- backend/src/api/middleware/authMiddleware.ts
+++ backend/src/api/middleware/authMiddleware.ts
@@ -1,23 +1,41 @@
 import { JwtTokenService } from "#infrastructure/services/Authetication/JwtToken.service.js";
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction, response } from "express";
 
 export const authMiddleware = (jwt: JwtTokenService) => {
     return (req: Request, res: Response, next: NextFunction) => {
         const authAutho = req.headers.authorization;
         
         if(!authAutho || !authAutho?.startsWith("Bearer")){
-            return res.status(401).send("HTTP 401 Unauthorized");
+            return res.status(401).send({
+                code: "NO_TOKEN",
+                response: "HTTP 401 Unauthorized"
+            });
         }
         
-        const [, token] = String(authAutho?.split(" "));
-        
-        const payload = jwt.verify(token);
+        try{
+            const [, token] = authAutho?.split(" ");
 
-        req.user = {
-            userId: Number(payload.userId),
-            usertype: payload.usertype
+            const payload = jwt.verify(token);
+    
+            req.user = {
+                userId: Number(payload.userId),
+                usertype: payload.usertype
+            }
+    
+            next();
+        } catch(error) {
+            if( error instanceof Error && error.name === "TokenExpiredError"){
+                return res.status(401).send({
+                    code: "TOKEN_EXPIRED",
+                    response: "Session expired. Please login again."
+                });
+            }
+
+            return res.status(401).send({
+                code: "INVALID_TOKEN",
+                response: "Invalid token."
+            });
         }
-
-        next();
+        
     }
 }
