c9f1085eb12e133d0c338a8d4da32fca397e0d7f
devYuno <annykessy20@gmail.com>
2026-07-30 17:05:57 -0300

    FIX: fixed something

--- backend/src/api/middleware/disciplineMiddleware.ts
+++ backend/src/api/middleware/disciplineMiddleware.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from "express"
 export const validateRegister = (req: Request, res: Response, next: NextFunction) => {
     // validates if the necessary data were provided to create discipline
     const { name, workload, areaID } = req.body
-    if(!name || !workload || !areaID)
+    if(!name || !areaID)
         return res.status(400).send({ response: `There are empty data`})
     next()
 }
